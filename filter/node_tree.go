18aaceb27244f5f13102463d8541d1e39b02f19d
beiluo <1210227080@qq.com>
2022-02-18 17:35:59 +0800

    add：添加对map和slice类型字段的解析操作，

--- filter/node_tree.go
+++ filter/node_tree.go
@@ -5,14 +5,29 @@ import (
 )
 
 type FieldNodeTree struct {
-	Key   string
-	Val   interface{}
+	Key string
+	//字段名
+
+	Val interface{}
+	//字段值，基础数据类型，int string，bool 等类型直接存在这里面，如果是struct类型则字段所有k v会存在Child里
+
+	IsSlice bool //是否是切片，或者数组，
+
+	//如果是struct则保存所有字段k v，如果是切片就保存切片的所有值
 	Child []*FieldNodeTree
 }
 
 func (t *FieldNodeTree) GetVal() interface{} {
 	if t.Child == nil {
 		return t.Val
+	}
+	if t.IsSlice { //为切片和数组时候key为空
+		slices := make([]interface{}, 0, len(t.Child))
+		for i := 0; i < len(t.Child); i++ {
+			slices = append(slices, t.Child[i].GetVal())
+		}
+		t.Val = slices
+		return slices
 	}
 	maps := make(map[string]interface{})
 	for _, v := range t.Child {
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"filter/filter"
+	"fmt"
+)
+
+type Book struct {
+	Page  string `json:"page,select(all|justBooks)"`
+	Title string `json:"title,select(all|justBooks)"`
+}
+type User struct {
+	Name string `json:"name,select(all)"`
+
+	Books []Book `json:"books,select(all|justBooks)"`
+
+	Map map[string]interface{} `json:"map,select(all|justMap)"`
+}
+
+func main() {
+	user := User{
+		Name: "boyan",
+		Map:  make(map[string]interface{}),
+	}
+
+	user.Map["book"] = Book{
+		Page:  "10",
+		Title: "golang",
+	}
+	user.Map["book-p"] = &Book{
+		Page:  "10",
+		Title: "golang",
+	}
+
+	user.Books = []Book{
+		{
+			Page:  "10",
+			Title: "golang",
+		},
+		{
+			Page:  "10",
+			Title: "golang",
+		},
+		{
+			Page:  "10",
+			Title: "golang",
+		},
+	}
+
+	user.Map["age"] = 10
+
+	user.Map["null_map"] = make(map[string]interface{})
+
+	fmt.Println(filter.SelectMarshal("all", user))
+	fmt.Println(filter.SelectMarshal("justMap", user))
+	fmt.Println(filter.SelectMarshal("justBooks", user))
+	//fmt.Println(filter.SelectMarshal("justMap", user))
+	//marshal, err := json.Marshal(user)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Println(string(marshal))
+
+}
