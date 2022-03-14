eb17c7778f35eae386063bab0d2a8e22e7680f06
liu-cn <1210227080@qq.com>
2022-03-14 17:37:06 +0800

    add： 添加对自定义time.time类型的支持 零值忽略

--- filter/time_test.go
+++ filter/time_test.go
@@ -1,18 +1,17 @@
 package filter
 
 import (
-	"encoding/json"
 	"fmt"
-	"reflect"
 	"testing"
 	"time"
 )
 
 type UserTime struct {
-	ID         int       `json:"id,select(list)"`
-	BirthTime  Time      `json:"birth_time,select(list)"`
-	BirthTime2 *Time     `json:"birth_time2,select(list)"`
-	Timer      time.Time `json:"timer,select($any)"`
+	ID           int       `json:"id,select(list)"`
+	BirthTime    Time      `json:"birth_time,select(list)"`
+	BirthTime2   *Time     `json:"birth_time2,select(list)"`
+	NilBirthTime Time      `json:"nil_birth_time,omitempty,select(list)"`
+	Timer        time.Time `json:"timer,select($any)"`
 }
 
 func TestTime(t *testing.T) {
@@ -23,23 +22,6 @@ func TestTime(t *testing.T) {
 		BirthTime2: (*Time)(&now),
 		Timer:      time.Now(),
 	}
-	v := reflect.ValueOf(Time{})
-	val, ok := v.Interface().(json.Marshaler)
-	fmt.Println(val, ok)
 
 	fmt.Println(SelectMarshal("list", user).MustJSON())
-
-	u2 := UserTime{}
-	marshal, err := json.Marshal(user)
-	if err != nil {
-		panic(err)
-	}
-
-	err = json.Unmarshal(marshal, &u2)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(u2)
-	fmt.Println(u2.BirthTime)
 }
