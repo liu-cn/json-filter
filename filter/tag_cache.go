c349a96d242980c96000e9eb8e56cbd9fa8f9b30
beiluo <1210227080@qq.com>
2022-12-18 23:57:30 +0800

    增加缓存，获得30-40%性能提升，默认开启缓存

---
+++ filter/tag_cache.go
@@ -0,0 +1,55 @@
+package filter
+
+var tagCache cache
+
+var enableCache = true
+
+func init() {
+	tagCache.c = make(map[string]tag)
+}
+
+type cache struct {
+	c map[string]tag
+}
+
+func (c *cache) key(pkgInfo string, scene string, fieldName string, isSelect bool) string {
+	s := ""
+	if !isSelect {
+		s = "omit." + scene + "."
+	} else {
+		s = "select." + scene + "."
+	}
+	return s + pkgInfo + "." + fieldName
+}
+
+func (c *cache) getTag(tagStr string, scene string, pkgInfo string, isSelect bool, fieldName string, omitNotTag *bool) (tagEl tag, find bool) {
+
+	key := c.key(pkgInfo, scene, fieldName, isSelect)
+
+	v, ok := c.c[key]
+
+	if !ok {
+		if tagStr != "" {
+			var tagE tag
+			if isSelect {
+				tagE = newSelectTag(tagStr, scene, fieldName)
+			} else {
+
+				if omitNotTag == nil {
+					return tag{}, false
+				}
+
+				if *omitNotTag {
+					tagE = newOmitNotTag(tagStr, fieldName)
+				} else {
+					tagE = newOmitTag(tagStr, scene, fieldName)
+				}
+			}
+			c.c[key] = tagE
+			return tagE, true
+		} else {
+			return tag{}, false
+		}
+	}
+	return v, true
+}
