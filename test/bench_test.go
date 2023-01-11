730b705ee5c36d1415d0cd06315313ee13907564
beiluo <1210227080@qq.com>
2023-01-11 17:47:11 +0800

    修复对interface{}类型的支持 ，解决代码冲突

--- test/bench_test.go
+++ test/bench_test.go
@@ -7,17 +7,17 @@ import (
 )
 
 type Users struct {
-	UID    uint   `json:"uid,select(article)"`    //select中表示选中的场景(该字段将会使用到的场景)
-	Avatar string `json:"avatar,select(article)"` //和上面一样此字段在article接口时才会解析该字段
+	UID    uint   `json:"uid,select(article),omit(article)"`    //select中表示选中的场景(该字段将会使用到的场景)
+	Avatar string `json:"avatar,select(article),omit(article)"` //和上面一样此字段在article接口时才会解析该字段
 
 	Nickname string `json:"nickname,select(article|profile)"` //"｜"表示有多个场景都需要这个字段 article接口需要 profile接口也需要
 
-	Sex        int       `json:"sex,select(profile)"`          //该字段是仅仅profile才使用
-	VipEndTime time.Time `json:"vip_end_time,select(profile)"` //同上
-	Price      string    `json:"price,select(profile)"`        //同上
+	Sex        int       `json:"sex,select(profile)"`                        //该字段是仅仅profile才使用
+	VipEndTime time.Time `json:"vip_end_time,select(profile),omit(article)"` //同上
+	Price      string    `json:"price,select(profile)"`                      //同上
 
-	Hobby string    `json:"hobby,omitempty,select($any)"` //任何场景下为空忽略
-	Lang  []LangAge `json:"lang,omitempty,select($any)"`  //任何场景下为空忽略
+	Hobby string    `json:"hobby,omitempty,select($any)"`              //任何场景下为空忽略
+	Lang  []LangAge `json:"lang,omitempty,select($any),omit(article)"` //任何场景下为空忽略
 }
 
 type LangAge struct {
@@ -48,29 +48,42 @@ func newUsers() Users {
 
 var str string
 
-func BenchmarkUserPointer(b *testing.B) {
+func BenchmarkOmitPointerWithCache(b *testing.B) {
 	user := newUsers()
+	filter.EnableCache(true)
 	for i := 0; i < b.N; i++ {
-		_ = filter.SelectMarshal("article", &user)
+		_ = filter.Omit("article", &user)
 	}
 }
 
-func BenchmarkUserPointerWithCache(b *testing.B) {
+func BenchmarkSelectPointerWithCache(b *testing.B) {
 	user := newUsers()
+	filter.EnableCache(true)
 	for i := 0; i < b.N; i++ {
 		_ = filter.Select("article", &user)
 	}
 }
 
-func BenchmarkUserVal(b *testing.B) {
+func BenchmarkOmitVal(b *testing.B) {
 	user := newUsers()
+	filter.EnableCache(false)
 	for i := 0; i < b.N; i++ {
-		_ = filter.SelectMarshal("article", user)
+		_ = filter.Omit("article", user)
 	}
 }
-func BenchmarkUserValWithCache(b *testing.B) {
+func BenchmarkSelectVal(b *testing.B) {
 	user := newUsers()
+	filter.EnableCache(false)
 	for i := 0; i < b.N; i++ {
 		_ = filter.Select("article", user)
+	}
+}
+
+func BenchmarkAll(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		b.Run("BenchmarkOmitPointerWithCache", BenchmarkOmitPointerWithCache)
+		b.Run("BenchmarkOmitVal", BenchmarkOmitVal)
+		b.Run("BenchmarkSelectPointerWithCache", BenchmarkSelectPointerWithCache)
+		b.Run("BenchmarkSelectVal", BenchmarkSelectVal)
 	}
 }
